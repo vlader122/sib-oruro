ef301d8e5d545b93a6c6195e2eebc98dbb7a30b5
Niver Copa <vlader122@gmail.com>
2023-10-24 21:46:59 -0400

    mi primer commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Landing from './components/Landing'
+import Noticias from './components/Noticias'
+import Facts from './components/Facts'
+import Features from './components/Features'
+Features
+function App() {
+  return (
+    <>
+    <Landing />
+    <Noticias />
+    <Facts />
+    </>
+  )
+}
+
+export default App
